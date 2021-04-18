d432501a54f883acfaec346e1cb99fbc3780d58e
OndrejFixmer <73097549+OndrejFixmer@users.noreply.github.com>
2021-04-18 19:31:42 +0200

    Add files via upload

---
+++ pexesicko/script.js
@@ -0,0 +1,34 @@
+
+//smajlici :)))))
+
+let cards = ["😂", "🥰", "🤔", "🙂", "😍", "😎", "😭", "🚀"]
+    let cardsdoubled = cards.concat(cards)
+        let board;
+        console.log(cardsdoubled)
+
+let init = function () {
+    board = document.getElementById('troll')
+        shuffleFunc()
+        htmlAdd()
+}
+
+let shuffleFunc = function () {
+    for (let i = 0; i < cardsdoubled.length; i++) {
+            let shuffle = Math.floor(Math.random() * i)
+            let j = cardsdoubled[i]
+        cardsdoubled[i] = cardsdoubled[shuffle]
+        cardsdoubled[shuffle] = j
+    }
+}
+
+let htmlAdd = function () {
+    for (let i = 0; i < cardsdoubled.length; i++) {
+            let div = document.createElement('div')
+            div.innerText = cardsdoubled[i]
+            div.className = 'karta'
+            board.appendChild(div)
+    }
+}
+
+
+window.onload = init
